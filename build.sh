9d78bcf132ab3462db383c021b9bf58b678a8516
Fabian Maurer <dark.shadow4@web.de>
2019-06-07 17:11:52 +0200

    Add simple DOS .com test

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ export WIPFC=$WATCOM/wipfc
 export INCLUDE=$WATCOM/h:$WATCOM/h/win
 export LIB=$WATCOM/lib286:$WATCOM/lib286/dos
 
-#i686-w64-mingw32-gcc -m16 -c test.c
+i686-w64-mingw32-gcc -m16 -c test.c -std=gnu99
+ld -m elf_i386 -T win16_ne.ld test.o -o test_self.exe --nmagic
 #strip test.o
 
-wasm -bt=windows -ms test2.asm
-wcl -s -zl -bcl=windows -c test.c
-wcl -bcl=windows test.o test2.o
+#wasm -bt=windows -ms test2.asm
+#wcl -s -zl -bcl=windows -c test.c
+#wcl -bcl=windows test.o test2.o
--- test.c
+++ test.c
@@ -1,14 +1,36 @@
-#include <windows.h>
+//#include <windows.h>
 
+asm (
+	"call  _dosmain\n"
+     "mov   $0x4C,%ah\n"
+     "int   $0x21\n"
+);
+
+static void print(char *string)
+{
+    asm volatile ("mov   $0x09, %%ah\n"
+                  "int   $0x21\n"
+                  : /* no output */
+                  : "d"(string)
+                  : "ah");
+}
+
+int dosmain(void)
+{
+    print("Hello, World2!\n$");
+    return 0;
+}
+
+/*
 void WinMainCRTStartup()
 {
 	// Windows doesn't run an exe < 376 byte in NE mode, but runs it as dos mdoe instead... Add a bit of "padding"...
 	char *message = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-	MessageBox(NULL, "success", "Info", MB_OK);
+	//MessageBox(NULL, "success", "Info", MB_OK);
 	_asm {
 		xor ax, ax
 		int 1ah
 		int 1ah
 		int 1ah
 	}
-}
+}*/
